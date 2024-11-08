cb8088eccaa0dcb98cd1fdc906800907dd175964
S H E R R Y <89315757+KenjIsanl@users.noreply.github.com>
2024-11-08 22:05:45 +0300

    Update task_2.py

--- лаба 3/task_2.py
+++ лаба 3/task_2.py
@@ -1,12 +1,12 @@
 def find_common_participants(str_1, str_2, split=","):
     list_1 = str_1.split(split)
     list_2 = str_2.split(split)
-    list_1_2 = list()
+    list1_2 = []
     for i in range(len(list_1)):
         for j in range(len(list_2)):
             if list_1[i] == list_2[j]:
-                list_1_2.append(list_1[i])
-    return list_1_2
+                list1_2.append(list_1[i])
+    return list1_2
 
 
 participants_first_group = "Иванов|Петров|Сидоров"
